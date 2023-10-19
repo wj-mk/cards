fbb08360c6abb30bd4f92e16fee321fae7a5c988
wj-mk <72305259+wj-mk@users.noreply.github.com>
2023-10-19 16:20:28 +0100

    Update if-else block to a switch statement

--- cards.c
+++ cards.c
@@ -10,21 +10,35 @@ int main()
     char card_name[3];
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
+    // int val = 0;
+    // if (card_name[0] == 'K') {
+    //     val = 10;
+    // } else if (card_name[0] == 'Q') {
+    //     val = 10;
+    // } else if (card_name[0] == 'J') {
+    //     val = 10;
+    // } else if (card_name[0] == 'A') {
+    //     val = 11;
+    // } else {
+    //     val = atoi(card_name);
+    // }
     int val = 0;
-    if (card_name[0] == 'K') {
-        val = 10;
-    } else if (card_name[0] == 'Q') {
-        val = 10;
-    } else if (card_name[0] == 'J') {
-        val = 10;
-    } else if (card_name[0] == 'A') {
-        val = 11;
-    } else {
-        val = atoi(card_name);
+    switch (card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        default:
+            val = atoi(card_name);
     }
     // Check if value is 3 to 6
     if (val > 2 && val < 7) {
         puts("Count has gone up");
+    // Otherwise check if the card was 10, J, Q, or K
     } else if (val == 10) {
         puts("Count has gone down");
     }
